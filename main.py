c7a7b21f110c7ad06d474d2bcd407f250c5fcbe6
godoy <nestor@chaindots.com>
2024-02-22 11:39:25 -0300

    add: Questions.md and fix

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ from src.kombat import CombatArena
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Un script simple de Python con argumentos de línea de comandos.')
-    parser.add_argument('--json', type=parse_json, default='', help='JSON específico')
+    parser.add_argument('--json_args', type=parse_json, default='', help='JSON específico')
     parser.add_argument('--file-json', type=str, default='', help='Ruta a un archivo JSON específico')
     args = parser.parse_args()
 
-    if not args.json and not args.file_json:
+    if not args.json_args and not args.file_json:
         parser.error('Se debe proporcionar uno de --json o --file-json.')
 
-    if args.json and args.file_json:
+    if args.json_args and args.file_json:
         parser.error('--json o --file-json son mutuamente excluentes')
 
     if args.file_json:
@@ -23,8 +23,8 @@ if __name__ == '__main__':
         file_json = args.file_json
         json_args = read_json(file_json)
     else:
-        # Handle args.file_json
-        json_args = args.json
+        # Handle args.json_args
+        json_args = args.json_args
 
     print()
 
